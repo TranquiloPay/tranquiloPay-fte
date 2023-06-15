c870bef3eebd67eecb82c032d20ff0d70f288e11
Patrick Arruda <Patrickarruda30@gmail.com>
2023-06-15 12:06:51 -0300

    fix: bug in service

--- src/constants/rest/rest.ts
+++ src/constants/rest/rest.ts
@@ -1,1 +1,4 @@
-export const TRANQUILOPAY_BFF_PATH = 'https://tranquilopay-bff.vercel.app';
+export const TRANQUILOPAY_BFF_PATH =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://tranquilopay-bff.vercel.app";
